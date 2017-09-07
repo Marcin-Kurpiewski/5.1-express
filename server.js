979d67ecfd70fba49ddbdd39e8669087e7599b5d
Marcin-Kurpiewski <kurus.mk@gmail.com>
2017-09-07 13:56:27 +0200

    endpoints

--- server.js
+++ server.js
@@ -2,10 +2,30 @@ var express = require('express');
 var app = express();
 
 
-app.get('/', function(req, res) {
-    res.send('Hello world');
+
+app.get('/:id', function(req, res) {
+    console.log("otrzymałem żondanie get od strony głównej");
+    res.send('Identyfikator który został dopisany to ' + req.params.id);
 });
 
-var server = app.listen(3000, function() {
-    console.log('Przykładowa aplikacja nasłuchuje na http://localhost:3000');
+app.listen(3000);
+
+app.post('/:', function (req, res) {
+    console.log('Otrzymałem żądanie POST do strony głównej');
+    res.send('Hello POST!');
+});
+
+app.delete('/:del_user', function (req, res) {
+    console.log('Otrzymałem żądanie DELETE do strony /del_user');
+    res.send('Hello DELETE!');
+});
+
+app.get('/:list_user', function (req, res) {
+    console.log('Otrzymałem żądanie GET do strony /list_user');
+    res.send('Strona z listą użytkowników!');
+});
+
+app.get('/:ab*cd', function(req, res) {
+    console.log('Otrzymałem żądanie GET do strony /ab*cd');
+    res.send('Wzór pasuje');
 });
