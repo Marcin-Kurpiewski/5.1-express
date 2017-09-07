1f65276a53fe321f2cb03da80dac210bd1267199
Marcin-Kurpiewski <kurus.mk@gmail.com>
2017-09-07 17:11:22 +0200

    server 5.3

--- server.js
+++ server.js
@@ -1,31 +1,29 @@
 var express = require('express');
 var app = express();
+var fs= require('fs');
+var bodyParser = require('body-parser');
+var stringifyFile='';
+
+app.use(bodyParser.json());
 
 
+app.get('/:getNote', function(req, res) {
+    fs.readFile('./test.json', 'utf8', function(err, data) {
+        if (err) throw err;
+        fileContent = data;
+        res.send(data);
+    });
+});
 
-app.get('/:id', function(req, res) {
-    console.log("otrzymałem żondanie get od strony głównej");
-    res.send('Identyfikator który został dopisany to ' + req.params.id);
+app.post('/updateNote/:note', function(req, res) {
+    fs.writeFile('./test.json', stringifyFile, function(req, res, next) {
+        if (err) throw err;
+        console.log('file updated');
+    });
+});
+
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!');
 });
 
 app.listen(3000);
-
-app.post('/:', function (req, res) {
-    console.log('Otrzymałem żądanie POST do strony głównej');
-    res.send('Hello POST!');
-});
-
-app.delete('/:del_user', function (req, res) {
-    console.log('Otrzymałem żądanie DELETE do strony /del_user');
-    res.send('Hello DELETE!');
-});
-
-app.get('/:list_user', function (req, res) {
-    console.log('Otrzymałem żądanie GET do strony /list_user');
-    res.send('Strona z listą użytkowników!');
-});
-
-app.get('/:ab*cd', function(req, res) {
-    console.log('Otrzymałem żądanie GET do strony /ab*cd');
-    res.send('Wzór pasuje');
-});
