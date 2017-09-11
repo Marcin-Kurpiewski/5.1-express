32d690f952ba78ffe3c4a507f436c0d4dbceb370
Marcin-Kurpiewski <kurus.mk@gmail.com>
2017-09-11 18:35:12 +0200

    pug/jade training

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
 app.use(express.static('assets'));
-
+app.set('view engine', 'pug');  // pug jako kreator widoków
+app.set('views','./views'); // widoki
 
 app.get('/', function (req, res) {
     res.sendFile('/index.html')
@@ -23,6 +24,30 @@ app.use(function(req, res, next){
 app.get('/store', function(req, res, next){
     console.log(" jestś w sklepie :-)  ");
     res.send("jesteś w sklepie");
+});
+
+app.get('/first-view', function(req, res){
+    res.render('first-view');
+});
+
+app.get('/dynamic', function(req, res){
+    res.render('dynamic', {
+        name: "moja dynamiczna strona",
+        url:"http://www.google.com"
+    });
+});
+
+app.get('/logic', function (req, res){
+    res.render('logic', {
+        user:
+            { name: "Johny", age:"20"}
+    });
+
+});
+
+
+app.get('/content', function (req, res){
+    res.render('content');
 });
 
 var server = app.listen(3000, function() {
